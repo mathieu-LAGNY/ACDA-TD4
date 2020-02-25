8e22d362e42b2ccb4902a434785ef79605bad742
mathieu-LAGNY <mathieu.lagny@etu.umontpellier.fr>
2020-02-25 01:21:07 +0100

    Squelette initial

---
+++ src/main/java/Racine.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Racine {
+    /**
+     * Calculer toutes les racines carrées des nombres compris entre {code A} et {code B}.
+     *
+     * @param A nombre entier tel que A < B.
+     * @param B nombre entier tel que A < B.
+     * @return un {@code List} contenant les racines carrées des nombres compris entre {code A} et {code B}.
+     */
+    public List<Integer> calculer(int A, int B) {
+        throw new RuntimeException("Not Implemented");
+    }
+}
