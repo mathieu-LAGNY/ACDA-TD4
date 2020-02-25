8a52bc1c8942dd69c978ede1a89091603f94d70f
mathieu-LAGNY <mathieu.lagny@etu.umontpellier.fr>
2020-02-25 11:40:34 +0100

    PalindromeTest

---
+++ src/main/java/Palindrome.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Palindrome {
+    /**
+     * Calculer le nombre de mots palindrômes dans une {code phrase} passée en paramètres.
+     *
+     * @param phrase une phrase.
+     * @return le nombre de mots palindrômes dans la {code phrase}.
+     */
+    public int calculer(String phrase) {
+        throw new RuntimeException("Not Implemented");
+    }
+}
